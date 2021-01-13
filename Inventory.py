fb6fce76be882225b22da39bd1d27c86813c629d
ltsimps <ltsimpso@me.com>
2021-01-13 16:09:21 -0500

    started basic inventory system

---
+++ Inventory.py
@@ -0,0 +1,19 @@
+@dataclass(frozen=True)
+class OrderLine:
+  orderid: str
+  sku: str
+  qty:int
+
+
+
+  class Batch:
+    def __init__(self, ref:str, sku: str, qty:int, eta:Optional[date]):
+      self.reference = ref
+      self.sky = sku
+      self.eta = eta
+      self.available_quantity = qty
+    
+  
+
+    def allocate(self, line:OrderLine):
+      self.available_quantity -= line.qty
